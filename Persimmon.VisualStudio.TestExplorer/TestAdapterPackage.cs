15acb427d22a7568c83b7ef32941dd3f0b7754c1
Kouji Matsui <k@kekyo.net>
2016-05-09 17:45:09 +0900

    Add VsPackage (WIP)

--- Persimmon.VisualStudio.TestExplorer/TestAdapterPackage.cs
+++ Persimmon.VisualStudio.TestExplorer/TestAdapterPackage.cs
@@ -8,24 +8,40 @@ using EnvDTE;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 
-using Persimmon.VisualStudio.TestExplorer;
-
-namespace persimmon_projects.Persimmon_VisualStudio_TestExplorer
+namespace Persimmon.VisualStudio.TestExplorer
 {
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the 
+    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// register itself and its components with the shell.
+    /// </remarks>
+    // TRAP: PackageRegistration.RegisterUsing is no effect. Must use UseCodebase element into csproj.
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [Guid(Constant.VisualStudioPkgIdString)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
-    public sealed class Persimmon_VisualStudio_TestExplorerPackage : Package
+    public sealed class TestAdapterPackage : Package
     {
-        public Persimmon_VisualStudio_TestExplorerPackage()
+        /// <summary>
+        /// Default constructor of the package.
+        /// </summary>
+        /// <remarks>
+        /// Inside this method you can place any initialization code that does not require 
+        /// any Visual Studio service because at this point the package object is created but 
+        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// initialization is the Initialize method.
+        /// </remarks>
+        public TestAdapterPackage()
         {
             Trace.WriteLine("TestAdapterPackage: constructed.");
-
-            Debug.Assert(false);
         }
-
         private Assembly LoadExtensionManager()
         {
             var dte = (DTE)this.GetService(typeof(DTE));
@@ -51,6 +67,10 @@ namespace persimmon_projects.Persimmon_VisualStudio_TestExplorer
             return (string)installPathProperty.GetValue(extensionManager, null);
         }
 
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
         protected override void Initialize()
         {
             Trace.WriteLine("TestAdapterPackage: Initialize(): enter.");
